d80c58c1640398f3433523ac35470af20b269d11
Selma Abbassi <selma.abbassi@lm.se>
2024-12-08 11:30:11 +0100

    Day 7 Part 1 (pro tip, don't use hashmap, it will be your demise...)

---
+++ day7/part1.py
@@ -0,0 +1,65 @@
+import itertools
+from typing import List
+
+class Equation:
+    def __init__(self, solution, numbers):
+         self.solution = solution
+         self.numbers = numbers
+
+    def is_valid_equation(self):
+        combinations = self.get_combinations()
+    
+        for combination in combinations:
+            result = self.numbers[0]
+            expression = f"{self.numbers[0]}"
+
+            for i, operator in enumerate(combination):
+                expression += f" {operator} {self.numbers[i+1]}"
+                if operator == '+':
+                    result += self.numbers[i+1]
+                elif operator == '*':
+                    result *= self.numbers[i+1]
+
+            if result == self.solution:
+                #print(f"Valid equation: {solution}={expression}, result={result}")
+                return True
+            else:
+                #print(f"Invalid equation: {solution}={expression}, result={result}")
+                continue
+    
+        return False
+
+    def get_combinations(self):
+        operators = ['*', '+']
+        if len(self.numbers) > 1:
+            return [list(combinations) for combinations in itertools.product(operators, repeat=len(self.numbers) - 1)]
+        else:
+            return []
+
+def parse_file():
+    equations :List[Equation] = []
+    
+    with open("day7.txt", "r") as file:
+        for line in file:
+            l = line.replace("\n", "")
+            t = l.split(":")
+            solution = int(t[0])
+            numbers = list(map(int, t[1].strip().split(" ")))
+            
+            equations.append(Equation(solution, numbers))
+            
+    return equations
+
+def main():
+    equations: List[Equation] = parse_file()
+    
+    counter = 0
+    
+    for equation in equations:
+        if equation.is_valid_equation():
+            counter += equation.solution
+            
+    print(f"Total calibration result: {counter}")
+
+if __name__ == '__main__':
+    main()
---
+++ day7/test_equation.py
@@ -0,0 +1,49 @@
+import unittest
+
+from part1 import Equation
+
+class TestMain(unittest.TestCase):
+    def test_get_combinations(self):
+        """Test combinations given list of numbers"""
+        eq = Equation(0, numbers = [1,2,3])
+        combinations = eq.get_combinations()
+        
+        self.assertEqual(combinations, [['*', '*'], ['*', '+'], ['+', '*'], ['+', '+']])
+        
+    def test_get_combinations_again(self):
+        """Test combinations given list of numbers"""
+        eq = Equation(0, numbers = [1,2])
+        combinations = eq.get_combinations()
+        
+        self.assertEqual(combinations, [['*'],['+']])
+        
+    def test_is_valid_equation_true(self):
+        """Test example equation is valid"""
+        eq = Equation(190, [10,19])
+        is_valid = eq.is_valid_equation()
+        
+        self.assertTrue(is_valid)
+        
+    def test_is_valid_equation_false(self):
+        """Test example equation is valid"""
+        eq = Equation(161011, [16,10,13])
+        is_valid = eq.is_valid_equation()
+        
+        self.assertFalse(is_valid)
+    
+    def test_is_valid_equation_left_to_right(self):
+        """Test example equation is valid"""
+        eq = Equation(292, [11,6,16,20])
+        is_valid = eq.is_valid_equation()
+        
+        self.assertTrue(is_valid)
+        
+    def test_is_valid_equation_another_example(self):
+        """Test example equation is valid"""
+        eq = Equation(4926689152, [20, 1, 1, 8, 2, 4, 532, 5, 4, 531])
+        is_valid = eq.is_valid_equation()
+        
+        self.assertFalse(is_valid)
+        
+if __name__ == '__main__':
+    unittest.main()
